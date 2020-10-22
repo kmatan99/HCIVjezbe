1e251fbf8c3d9161ecbe082a86f6be4d8d3dadbc
Karlo <kmatan01@fesb.hr>
2020-10-22 10:59:08 +0200

    Adding Navigation

--- src/components/navigationHeader/index.js
+++ src/components/navigationHeader/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import styles from './style.module.css'
 import Logo from '../logo/index.js'
+import Navigation from '../navigation/index.js'
 
 const NavigationHeader = () => (
   <section className={styles.navigationHeader}>
     <Logo />
+    <Navigation />
   </section>
 )
 export default NavigationHeader
