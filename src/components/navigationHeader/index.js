2572fef35652c3384fee184f8f23e48cc57ded51
Karlo <kmatan01@fesb.hr>
2020-10-22 10:46:28 +0200

    Adding Logo

--- src/components/navigationHeader/index.js
+++ src/components/navigationHeader/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import styles from './style.module.css'
+import Logo from '../logo/index.js'
 
 const NavigationHeader = () => (
   <section className={styles.navigationHeader}>
+    <Logo />
   </section>
 )
 export default NavigationHeader
