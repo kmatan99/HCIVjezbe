66f40071d98d6ff962a504c53f37c0a00457d182
Karlo <kmatan01@fesb.hr>
2020-11-05 13:44:58 +0100

    Contact page ContactForm

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,13 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
- 
+import ContactSeparator from '../modules/contactSeparator'
+import ContactForm from '../modules/contactForm'
+
 const ContactPage = () => (
     <HeaderFooterLayout activeTab="Contact">
+        <ContactSeparator />
+        <ContactForm />
     </HeaderFooterLayout>
 )
- 
+
 export default ContactPage
